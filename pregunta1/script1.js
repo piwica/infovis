f67e7b1862aed5468ed42efa428c1da1d6229b2b
piwica <piwica@gmail.com>
2014-06-28 13:43:19 -0300

    Add future leadership

--- pregunta1/script1.js
+++ pregunta1/script1.js
@@ -2,7 +2,7 @@ var width = 600,
     height = 400;
 var svg = d3.select("body").append("svg")
     .attr("width", width+300)
-    .attr("height", height);
+    .attr("height", height+100);
 
 // Arrows markers
 
@@ -196,6 +196,27 @@ svg.append("line")
     .attr("y2",3*height/4-2)
     .attr("marker-end","url(#markerEnd)");
 
+//Create Silvia_Marek and Mandor_Vann
+var Silvia_Marek = svg
+    .append("rect")
+    .attr("x", width/6)
+    .attr("y", 4*height/4)
+    .attr("width", 100)
+    .attr("height", 40)
+    .attr("rx", 10)
+    .attr("ry", 10)
+    .style("fill", "white");
+
+var Mandor_Vann = svg
+    .append("rect")
+    .attr("x", 4*width/6)
+    .attr("y", 4*height/4)
+    .attr("width", 100)
+    .attr("height", 40)
+    .attr("rx", 10)
+    .attr("ry", 10)
+    .style("fill", "white");
+
 // Create text
 
 var textplace= svg
@@ -233,7 +254,9 @@ Henk_Bodrogi.on("click", function() {
         texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
             unparsedData+ '</div>')
     });
-
+    svg.selectAll(".invisible").data([]).exit().remove();
+    Silvia_Marek.style("fill", "white");
+    Mandor_Vann.style("fill", "white");
     d3.event.stopPropagation();
 });
 
@@ -252,6 +275,9 @@ Carmine_Osvaldo.on("click", function() {
         texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
             unparsedData+ '</div>')
     });
+    svg.selectAll(".invisible").data([]).exit().remove();
+    Silvia_Marek.style("fill", "white");
+    Mandor_Vann.style("fill", "white");
     d3.event.stopPropagation();
 });
 
@@ -270,6 +296,86 @@ Elain_Karel.on("click", function() {
         texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
             unparsedData+ '</div>')
     });
+    svg.selectAll(".invisible").data([]).exit().remove();
+
+    Silvia_Marek.transition()
+        .style("fill", "steelblue");
+
+    Mandor_Vann.transition()
+        .style("fill", "steelblue");
+
+    svg.append("line")
+        .attr("class", "invisible futureleadership")
+        .attr("x1", 0 )
+        .attr("y1", 3.75*height/4)
+        .attr("x2", width )
+        .attr("y2", 3.75*height/4);
+
+    svg.append("text")
+        .attr("class", "invisible futureleadership")
+        .style("stroke", "none")
+        .attr("x", 0 )
+        .attr("y", 3.75*height/4)
+        .attr("dy", "20px")
+        .attr("dx", "20px")
+        .text("Future Leadership");
+
+    svg.append("text")
+        .attr("class", "invisible")
+        .attr("x", width/6 )
+        .attr("y", 4*height/4)
+        .attr("dy", "25px")
+        .attr("dx", "50px")
+        .style("text-anchor", "middle")
+        .text("Silvia Marek");
+
+    svg.append("g")
+        .append("path")
+        .attr("class", "link invisible")
+        .attr("marker-end","url(#markerEnd)")
+        .attr("d", function() {
+            var dx = (2.5*width/6+50) - (width/6+100),
+                dy =  (3*height/4+40)- (4*height/4+20),
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" +
+                (2.5*width/6+50) + "," +
+                (3*height/4+40)+
+                "A" +
+                dr + "," + dr + " 0 0,1 " +
+                (width/6+100) + "," +
+                (4*height/4+20)
+                ;
+        });
+
+    svg.append("text")
+        .attr("class", "invisible")
+        .attr("x", 4*width/6 )
+        .attr("y", 4*height/4)
+        .attr("dy", "25px")
+        .attr("dx", "50px")
+        .style("text-anchor", "middle")
+        .text("Mandor Vann");
+
+    svg.append("g")
+        .append("path")
+        .attr("class", "link invisible")
+        .attr("marker-start","url(#markerStart)")
+        .attr("stroke-dasharray", "10,10")
+        .attr("d", function() {
+            var dx = (2.5*width/6+50) - (4*width/6),
+                dy =  (3*height/4+40)- (4*height/4+20),
+                dr = Math.sqrt(dx * dx + dy * dy);
+            return "M" +
+                (4*width/6) + "," +
+                (4*height/4+20)+
+                "A" +
+                dr + "," + dr + " 0 0,1 " +
+                (2.5*width/6+50) + "," +
+                (3*height/4+40)
+                ;
+        });
+
+
     d3.event.stopPropagation();
 });
 
@@ -285,6 +391,49 @@ Jeroen_Karel.on("click", function() {
 
     textplace.transition().style("fill", "#23415A");
     d3.text("jKarel.html", function(unparsedData){
+        texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
+            unparsedData+ '</div>')
+    });
+    svg.selectAll(".invisible").data([]).exit().remove();
+    Silvia_Marek.style("fill", "white");
+    Mandor_Vann.style("fill", "white");
+    d3.event.stopPropagation();
+});
+
+Mandor_Vann.on("click", function() {
+    Henk_Bodrogi.transition()
+        .style("fill", "steelblue");
+    Elain_Karel.transition()
+        .style("fill", "steelblue");
+    Carmine_Osvaldo.transition()
+        .style("fill", "steelblue");
+    Silvia_Marek.transition()
+        .style("fill", "steelblue");
+    Mandor_Vann.transition()
+        .style("fill", "#23415A");
+
+    textplace.transition().style("fill", "#23415A");
+    d3.text("vann.html", function(unparsedData){
+        texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
+            unparsedData+ '</div>')
+    });
+    d3.event.stopPropagation();
+});
+
+Silvia_Marek.on("click", function() {
+    Henk_Bodrogi.transition()
+        .style("fill", "steelblue");
+    Elain_Karel.transition()
+        .style("fill", "steelblue");
+    Carmine_Osvaldo.transition()
+        .style("fill", "steelblue");
+    Mandor_Vann.transition()
+        .style("fill", "steelblue");
+    Silvia_Marek.transition()
+        .style("fill", "#23415A");
+
+    textplace.transition().style("fill", "#23415A");
+    d3.text("marek.html", function(unparsedData){
         texttitle.html('<div class="text" style="width: 290px; color:white; font: 12px sans-serif;">' +
             unparsedData+ '</div>')
     });
